6edb9941b6c9ceef6c152538692320684ed07604
Anton Boyko <50466992+gittoha@users.noreply.github.com>
2021-01-29 16:42:29 +0200

    small refubrished 4.8

--- lib/ui/screen/sight_card.dart
+++ lib/ui/screen/sight_card.dart
@@ -15,80 +15,85 @@ class SightCard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      height: 188,
-      margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
-      decoration: BoxDecoration(
-        borderRadius: BorderRadius.circular(16),
-        color: Color(0xFFF5F5F5),
-      ),
-      child: Column(
-        children: [
-          Expanded(
-            flex: 1,
-            child: Stack(
-              children: [
-                Container(
-                  decoration: BoxDecoration(
-                    color: Colors.yellow,
-                    borderRadius: BorderRadius.vertical(
-                      top: Radius.circular(16),
+    return AspectRatio(
+      aspectRatio: 3 / 2,
+      child: Container(
+        height: 188,
+        margin: const EdgeInsets.symmetric(vertical: 8, horizontal: 16),
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(16),
+          color: Color(0xFFF5F5F5),
+        ),
+        child: Column(
+          children: [
+            Expanded(
+              flex: 1,
+              child: Stack(
+                children: [
+                  Container(
+                    decoration: BoxDecoration(
+                      color: Colors.yellow,
+                      borderRadius: BorderRadius.vertical(
+                        top: Radius.circular(16),
+                      ),
                     ),
                   ),
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(16),
-                  child: Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        sight.type,
-                        style: smallBoldTextStyle(color: white),
-                      ),
-                      Container(
-                        width: 20,
-                        height: 18,
-                        color: white,
-                      ),
-                    ],
+                  Padding(
+                    padding: const EdgeInsets.all(16),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          sight.type,
+                          style: smallBoldTextStyle(color: white),
+                        ),
+                        Container(
+                          width: 20,
+                          height: 18,
+                          color: white,
+                        ),
+                      ],
+                    ),
                   ),
-                ),
-              ],
+                ],
+              ),
             ),
-          ),
-          Expanded(
-            flex: 1,
-            child: Padding(
-              padding: const EdgeInsets.only(left: 16, bottom: 16),
-              child: Align(
-                alignment: Alignment.centerLeft,
-                child: ConstrainedBox(
-                  constraints: BoxConstraints(
-                    maxWidth: 250,
-                  ),
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      const SizedBox(height: 16),
-                      Text(
-                        sight.name,
-                        style: smallTitleTextStyle(color: secondary),
-                        maxLines: 2,
-                      ),
-                      const SizedBox(height: 2,),
-                      Text(
-                        sight.details,
-                        style: smallTextStyle(color: secondary2),
-                        overflow: TextOverflow.ellipsis,
-                        maxLines: 2,
-                      ),
-                    ],
+            Expanded(
+              flex: 1,
+              child: Padding(
+                padding: const EdgeInsets.only(left: 16, bottom: 16),
+                child: Align(
+                  alignment: Alignment.centerLeft,
+                  child: ConstrainedBox(
+                    constraints: BoxConstraints(
+                      maxWidth: 250,
+                    ),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const SizedBox(height: 16),
+                        Text(
+                          sight.name,
+                          style: smallTitleTextStyle(color: secondary),
+                          maxLines: 2,
+                        ),
+                        const SizedBox(
+                          height: 2,
+                        ),
+                        Text(
+                          sight.details,
+                          style: smallTextStyle(color: secondary2),
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 2,
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
